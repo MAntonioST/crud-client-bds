ceeb284c5734b4bbb837d2fc73a4bc7f39a31728
Marco Antonio <marco.teixeira4@fatec.sp.gov.br>
2021-01-17 09:51:18 -0300

    Initial project

---
+++ crudbds/src/test/java/com/client/crudbds/CrudbdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.client.crudbds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudbdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
